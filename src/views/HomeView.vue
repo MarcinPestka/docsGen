ef6da46f97480856ad3d02605df6e6200647028f
Marcin Pestka <pestka.m.j@gail.com>
2024-05-05 10:23:26 +0200

    Working input wrapper component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import inputWrapper from '@/components/form_inputs/inputWrapper.vue'
+</script>
 
 <template>
   <p>essa</p>
+  <inputWrapper :input-type="1" label="Test input" :validaiton="false"></inputWrapper>
 </template>
